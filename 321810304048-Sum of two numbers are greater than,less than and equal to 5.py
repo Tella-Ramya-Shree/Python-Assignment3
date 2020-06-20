ad564fbd20f7535762beeb371e8e25610b1c1272
Tella Ramya Shree <67000854+Tella-Ramya-Shree@users.noreply.github.com>
2020-06-20 16:16:42 +0530

    Add files via upload

---
+++ 321810304048-Sum of two numbers are greater than,less than and equal to 5.py
@@ -0,0 +1,7 @@
+#Sum of two numbers are greater than,less than and equal to 5
+first = int(input("Enter first number:"))
+second = int(input("Enter second number:"))
+sum = first+second
+print ("Sum is greater than 5:",sum>5)
+print ("Sum is equal to 5:",sum==5)
+print ("Sum is lesser than 5:",sum<5)
